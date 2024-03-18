a49adacbbef15165b2b3448690dd3a5cacae58bd
Alex Yong <alexjyong@gmail.com>
2024-03-18 13:15:38 -0400

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,18 +1,13 @@
 import os
-import psycopg2
+import sqlite3
 
 from os.path import join
 BUILD_PATH = join(".", "db", "build.sql")
 
+DB_PATH = "your_database_name.db"
 
-try: DATABASE_URL = os.environ['DATABASE_URL']
-except KeyError: DATABASE_URL = None
-
-conn = psycopg2.connect(DATABASE_URL)
-
-# cxn = connect(DB_PATH, check_same_thread=False)
+conn = sqlite3.connect(DB_PATH)
 cur = conn.cursor()
-
 
 def with_commit(func):
     def inner(*args, **kwargs):
@@ -21,65 +16,52 @@ def with_commit(func):
         return v
     return inner
 
-
 @with_commit
 def build():
     scriptexec(BUILD_PATH)
-
 
 def close():
     cur.close()
     conn.close()
 
-
 def commit():
     conn.commit()
 
-
 def field(command, *values):
     cur.execute(command, tuple(values))
-
-    if (fetch := cur.fetchone()) is not None:
+    fetch = cur.fetchone()
+    if fetch is not None:
         return fetch[0]
-
 
 def record(command, *values):
     cur.execute(command, tuple(values))
-
     return cur.fetchone()
-
 
 def records(command, *values):
     cur.execute(command, tuple(values))
-
     return cur.fetchall()
-
 
 def column(command, *values):
     cur.execute(command, tuple(values))
-
     return [item[0] for item in cur.fetchall()]
 
-
 def execute(command, *values):
-    return cur.execute(command, tuple(values))
-
+    cur.execute(command, tuple(values))
 
 def executeF1(command, *values):
     cur.execute(command, tuple(values))
     return cur.fetchone()
 
-
 def executeFAll(command, *values):
     cur.execute(command, tuple(values))
     return cur.fetchall()
 
-
-def multiexec(command, valueset):
-    return cur.executemany(command, valueset)
-
-
 def scriptexec(path):
     with open(path, "r", encoding="utf-8") as script:
-        cur.execute(script.read())
-    print("PG Ran", path)
+        script_content = script.read()
+        # Split the script into separate statements on each semicolon
+        statements = script_content.split(';')
+        for statement in statements:
+            if statement.strip():  # Make sure it's not an empty statement
+                cur.execute(statement)
+    print("SQLite Ran", path)
