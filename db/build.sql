00fc0293611a3a69d15923d309843fb8044188ac
Alex Yong <alexjyong@gmail.com>
2024-03-18 13:12:52 -0400

    updating to use sqllite

--- db/build.sql
+++ db/build.sql
@@ -1,25 +1,23 @@
 CREATE TABLE IF NOT EXISTS Prefix (
-    GuildID BIGINT PRIMARY KEY,
+    GuildID INTEGER PRIMARY KEY,
     Prefix TEXT DEFAULT '+'
 );
 
 CREATE TABLE IF NOT EXISTS Votes (
-    VoteID BIGSERIAL PRIMARY KEY,
-    CreatorID BIGINT,
+    VoteID INTEGER PRIMARY KEY AUTOINCREMENT,
+    CreatorID INTEGER,
     Question TEXT DEFAULT 'Poll',
     VoteLimit INTEGER DEFAULT 0,
-    GuildID BIGINT NOT NULL,
-    ChannelID BIGINT NOT NULL,
+    GuildID INTEGER NOT NULL,
+    ChannelID INTEGER NOT NULL,
     PollStage INTEGER NOT NULL,
     Type INTEGER NOT NULL,
     NumWinners INTEGER DEFAULT 1,
-    CreationDate TIMESTAMP DEFAULT NOW()
+    CreationDate DATETIME DEFAULT CURRENT_TIMESTAMP
 );
--- Stage: 0 = Created, 1 = Posted, 2 = Reactions added - running, -1 = Finished
--- Type: 0 = Quick Poll, 1 = Standard Poll, 2 = STV Poll
 
 CREATE TABLE IF NOT EXISTS Options (
-    VoteID BIGINT,
+    VoteID INTEGER,
     OptionNumb INTEGER,
     Prompt TEXT NOT NULL,
     PRIMARY KEY (VoteID, OptionNumb),
@@ -27,16 +25,16 @@ CREATE TABLE IF NOT EXISTS Options (
 );
 
 CREATE TABLE IF NOT EXISTS VoteMessages (
-    VoteID BIGINT,
-    MessageID BIGINT,
+    VoteID INTEGER,
+    MessageID INTEGER,
     Part INTEGER NOT NULL,
     PRIMARY KEY (MessageID),
     FOREIGN KEY (VoteID) REFERENCES Votes(VoteID) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS UserVote (
-    VoteID BIGINT,
-    UserID BIGINT,
+    VoteID INTEGER,
+    UserID INTEGER,
     Choice INTEGER NOT NULL,
     Preference INTEGER NOT NULL,
     PRIMARY KEY (VoteID, UserID, Choice),
